e4423d5754429016a32432b22ad8bc781d328188
Pieter Algera <pieteralgera@ymail.com>
2019-12-27 22:32:42 +0100

    Added EasyMobile

---
+++ Assets/EasyMobile/Editor/EM_Constants_Version.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EasyMobile.Editor
+{
+    public static partial class EM_Constants
+    {
+        // Product variant
+        public const string ProductVariant = "Pro";
+
+        // Current version
+        public const string versionString = "2.5.0";
+        public const int versionInt = 0x020500;
+    }
+}
+
