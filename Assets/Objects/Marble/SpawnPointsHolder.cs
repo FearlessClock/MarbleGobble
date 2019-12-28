7b45b0b8af993df08f8f9c0d560f3d06e504fc87
Pieter Algera <pieteralgera@ymail.com>
2019-12-28 13:23:38 +0100

    Added Spawn points holder and spawn point struct

---
+++ Assets/Objects/Marble/SpawnPointsHolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SpawnPoint
+{
+    public int ID;
+    public Vector2 spawnpoint;
+    public float angleFromRight;
+}
+[CreateAssetMenu(fileName = "New Spawn Point Holder", menuName = "MarbleGobble/Spawn Point Holder")]
+public class SpawnPointsHolder : ScriptableObject
+{
+    private List<SpawnPoint> listOfSpawnPoints = null;
+    public Action OnValueChanged; 
+    
+    public List<SpawnPoint> ListOfSpawnPoints
+    {
+        get { return listOfSpawnPoints; }
+        set { listOfSpawnPoints = value; OnValueChanged?.Invoke(); }
+    }
+}
