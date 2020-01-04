1e457182b83b5bb866f98b0dc31c4c4c401c07e7
Pieter Algera <pieteralgera@ymail.com>
2020-01-04 14:45:34 +0100

    Added camera rescaler

---
+++ Assets/HelperScripts/CameraRescaler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraRescaler : MonoBehaviour
+{
+    private Camera currentCamera = null;
+    [SerializeField] private float sceneWidth = 8f;
+    private void Awake()
+    {
+        currentCamera = GetComponent<Camera>();
+        if (currentCamera != null)
+        {
+            Debug.Log("No camera on the object");
+        }
+    }
+    private void Update()
+    {
+        currentCamera.orthographicSize = 0.5f * (sceneWidth / Screen.width) * Screen.height;
+    }
+}
